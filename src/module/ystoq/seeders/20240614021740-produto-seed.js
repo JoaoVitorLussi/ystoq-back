4295448a17617259bfd3f8a205a1e3dcf16aa7a0
Pedro Henrique Knebel <pedrohenriqueknebel@gmail.com>
2024-06-17 01:28:12 -0300

    seeders completas

--- src/module/ystoq/seeders/20240614021740-produto-seed.js
+++ src/module/ystoq/seeders/20240614021740-produto-seed.js
@@ -5,6 +5,14 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     const table = {tableName: 'produto', schema: "public"};
     await queryInterface.bulkInsert(table, [{
+      descricao: 'Arroz',
+      id_categoria_produto: 1,
+      quantidade: 100,
+      id_empresa: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
       descricao: 'Coca Cola',
       id_categoria_produto: 2,
       quantidade: 100,
@@ -13,17 +21,65 @@ module.exports = {
       updatedAt: new Date()
     },
     {
-      descricao: 'Batedeira',
+      descricao: 'Geladeira',
       id_categoria_produto: 3,
+      quantidade: 50,
+      id_empresa: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      descricao: 'Smartphone',
+      id_categoria_produto: 4,
+      quantidade: 200,
+      id_empresa: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      descricao: 'Sofá',
+      id_categoria_produto: 5,
       quantidade: 20,
       id_empresa: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
+      descricao: 'Camiseta',
+      id_categoria_produto: 6,
+      quantidade: 300,
+      id_empresa: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      descricao: 'Tênis',
+      id_categoria_produto: 7,
+      quantidade: 150,
+      id_empresa: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
       descricao: 'Cubo Mágico',
-      id_categoria_produto: 1,
-      quantidade: 25,
+      id_categoria_produto: 8,
+      quantidade: 120,
+      id_empresa: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      descricao: 'Livro de Ficção',
+      id_categoria_produto: 9,
+      quantidade: 80,
+      id_empresa: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      descricao: 'Perfume',
+      id_categoria_produto: 10,
+      quantidade: 60,
       id_empresa: 1,
       createdAt: new Date(),
       updatedAt: new Date()
