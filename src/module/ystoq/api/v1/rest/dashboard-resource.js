28a38e6e0d7070097a045b5d4928df1e6d27adf6
joaolussi <joaolussi@unochapeco.edu.br>
2024-06-17 19:29:28 -0300

    ajustes

--- src/module/ystoq/api/v1/rest/dashboard-resource.js
+++ src/module/ystoq/api/v1/rest/dashboard-resource.js
@@ -12,7 +12,10 @@ const sequelize = new Sequelize('ystoq', 'postgres', 'postgres', {
 
 router.get("/dashboard-entrada-saida", authMiddleware, async function (req, resp) {
   try {
-    const query = `WITH entrada AS (SELECT EXTRACT(MONTH FROM data) AS mes,
+    const email = req.email;
+    let query;
+    if(email === 'adminystoq@ystoq.com'){
+    query = `WITH entrada AS (SELECT EXTRACT(MONTH FROM data) AS mes,
                         SUM(quantidade)          AS total_entrada
                  FROM movimentacao_estoque
                  WHERE tipo = 0
@@ -30,6 +33,41 @@ router.get("/dashboard-entrada-saida", authMiddleware, async function (req, resp
         LEFT JOIN entrada e ON m.mes = e.mes
         LEFT JOIN saida s ON m.mes = s.mes
             ORDER BY m.mes;`;
+    }else{
+        query = `WITH usuario_cte AS (
+                SELECT u.id_empresa
+                FROM usuario u
+                WHERE u.email ~* '${email}'
+            ),
+            entrada AS (
+                SELECT EXTRACT(MONTH FROM me.data) AS mes,
+                    SUM(me.quantidade) AS total_entrada
+                FROM movimentacao_estoque me
+                JOIN estoque e ON me.id_estoque = e.id
+                WHERE me.tipo = 0
+                AND e.id_empresa = (SELECT id_empresa FROM usuario_cte)
+                GROUP BY mes
+            ),
+            saida AS (
+                SELECT EXTRACT(MONTH FROM me.data) AS mes,
+                    SUM(me.quantidade) AS total_saida
+                FROM movimentacao_estoque me
+                JOIN estoque e ON me.id_estoque = e.id
+                WHERE me.tipo = 1
+                AND e.id_empresa = (SELECT id_empresa FROM usuario_cte)
+                GROUP BY mes
+            ),
+            meses AS (
+                SELECT generate_series(1, 12) AS mes
+            )
+            SELECT m.mes,
+                COALESCE(e.total_entrada, 0) AS entrada,
+                COALESCE(s.total_saida, 0) AS saida
+            FROM meses m
+            LEFT JOIN entrada e ON m.mes = e.mes
+            LEFT JOIN saida s ON m.mes = s.mes
+            ORDER BY m.mes;
+`}       
     const dados = await sequelize.query(query, {
       type: QueryTypes.SELECT,
     });
@@ -44,13 +82,25 @@ router.get("/dashboard-quantidade-estoque", authMiddleware, async function (req,
     try {
         let estoques;
         let dados;
-        const query = `select e.descricao from estoque e  order by e.descricao;`;
+        let email = req.email;
+        let sql;
+        let query;
+        if(email === 'adminystoq@ystoq.com'){
+        query = `select e.descricao from estoque e
+                        order by e.descricao;`;
+        }else{
+            query = `select e.descricao from estoque e
+                        left join usuario on usuario.id_empresa = e.id_empresa
+                            where usuario.email ~* '${email}'
+                        order by e.descricao;`;
+        }                
 
         estoques = await sequelize.query(query, {
             type: QueryTypes.SELECT,
         });
 
-        let sql = `WITH estoque_produto_cte AS (SELECT p.id                       AS id_produto,
+        if(email === 'adminystoq@ystoq.com'){
+        sql = `WITH estoque_produto_cte AS (SELECT p.id                       AS id_produto,
                                     p.descricao                AS nome_produto,
                                     e.descricao                AS nome_estoque,
                                     COALESCE(ep.quantidade, 0) AS quantidade
@@ -63,6 +113,24 @@ router.get("/dashboard-quantidade-estoque", authMiddleware, async function (req,
                     GROUP BY nome_produto
                     HAVING SUM(CASE WHEN quantidade > 0 THEN 1 ELSE 0 END) > 0
                     ORDER BY nome_produto;`
+        }else{
+            sql = `WITH estoque_produto_cte AS (
+                    SELECT p.id AS id_produto,
+                        p.descricao AS nome_produto,
+                        e.descricao AS nome_estoque,
+                        COALESCE(ep.quantidade, 0) AS quantidade
+                    FROM produto p
+                    CROSS JOIN estoque e
+                    LEFT JOIN estoque_produto ep ON ep.id_estoque = e.id AND ep.id_produto = p.id
+                    JOIN usuario u ON u.id_empresa = e.id_empresa
+                    WHERE u.email ~* '${email}')
+                SELECT nome_produto AS name,
+                    ARRAY_AGG(quantidade ORDER BY nome_estoque) AS data
+                FROM estoque_produto_cte
+                GROUP BY nome_produto
+                HAVING SUM(CASE WHEN quantidade > 0 THEN 1 ELSE 0 END) > 0
+                ORDER BY nome_produto;`
+        }
 
         dados = await sequelize.query(sql, {
             type: QueryTypes.SELECT,
